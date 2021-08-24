16e62586ded994175750cac12887fe7fde706f47
MrBubb1es <lance04preston@gmail.com>
2021-08-23 21:44:08 -0500

    Completely changed input system
    pause thing will probably need to be changed

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -2,13 +2,18 @@ extends KinematicBody
 
 onready var camera = $Camera
 
+export var gravity = 100.0
+export(float, 0, 0.01) var turn_speed = .005
+export var fire_force = 30.0
+export(float, 0, 1) var friction = .9
+
 var velocity = Vector3.ZERO
-var gravity = 100.0
-var turn_speed = 5.0
 var fire_vector = Vector3(0, 1, -1).normalized()
-var fire_force = 25.0
 # 1.0 is no friction, 0.0 is infinite friction
-var friction = .9
+var paused = false
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # find the vector perpendicular to a normalized vector on the x and z axis
 func calculate_perpendicular_vector(normal):
@@ -19,6 +24,8 @@ func calculate_perpendicular_vector(normal):
 
 
 func _physics_process(delta):
+	"""
+	# OLD INPUT SYSTEM
 	if Input.is_action_pressed("ui_left"):
 		fire_vector = fire_vector.rotated(Vector3.UP, turn_speed * delta)
 		#camera.rotate_y(turn_speed * delta)
@@ -33,20 +40,35 @@ func _physics_process(delta):
 		fire_vector = fire_vector.rotated(perpendicular_vector, turn_speed * delta)
 	elif Input.is_action_pressed("ui_down"):
 		fire_vector = fire_vector.rotated(perpendicular_vector, -turn_speed * delta)
+	"""
 	
-	if Input.is_action_just_pressed("fire") and is_on_floor():
-		velocity += fire_vector * fire_force
+	if Input.is_action_just_pressed("ui_cancel"):
+		paused = !paused
+		if paused:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	if !paused:
+		if Input.is_action_just_pressed("fire") and is_on_floor():
+			velocity += fire_vector * fire_force
 		
-	camera.translation = Vector3(perpendicular_vector.x * -10, 3, abs(perpendicular_vector.z) * -10)
-	camera.look_at(transform.origin, Vector3.UP)
+		# 2d vector to position the camera while ignoring the y dimension of the fire_vector
+		var look_vector = Vector2(fire_vector.x, fire_vector.z).normalized()
+		camera.translation = Vector3(look_vector.x * -10, 3, look_vector.y * -10)
+		camera.look_at(transform.origin, Vector3.UP)
+		
+		if is_on_floor():
+			velocity.x *= friction
+			velocity.z *= friction
+		
+		velocity.y -= gravity * delta
+		velocity = move_and_slide(velocity, Vector3.UP)
 	
-	print(camera.translation)
 	
-	if is_on_floor():
-		velocity.x *= friction
-		velocity.z *= friction
-	
-	velocity.y -= gravity * delta
-	velocity = move_and_slide(velocity, Vector3.UP)
-	
-	
+func _input(event):
+	if !paused:
+		# If the player moves the mouse
+		if event is InputEventMouseMotion:
+			fire_vector = fire_vector.rotated(Vector3.UP, event.relative.x * turn_speed)
+			fire_vector = fire_vector.rotated(calculate_perpendicular_vector(fire_vector), event.relative.y * turn_speed)
